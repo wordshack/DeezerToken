ff917664e178954d6601aa0f0d9bddd1f4a509e8
wordshack <wordshack04@gmail.com>
2019-05-31 03:04:09 -0500

    DeezerToken
    DeezerToken

--- app/src/main/java/ws/deezertk/MainActivity.java
+++ app/src/main/java/ws/deezertk/MainActivity.java
@@ -2,6 +2,8 @@ package ws.deezertk;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected WebView web;
     protected EditText edToken;
     private static final String urlDeezer = "https://www.deezer.com/mx/login";
-    private static final String USERAGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22";
+    private static final String USERAGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
     private static final String COOKIE_TOKEN = "arl";
     protected String Cookie,Token;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         web = findViewById(R.id.web);
         edToken = findViewById(R.id.edTextoToken);
+        prefs = getSharedPreferences("save", Context.MODE_PRIVATE);
+        Token = prefs.getString("token", "");
+
+        edToken.setText(Token);
 
         web.getSettings().setUserAgentString(USERAGENT);
         web.getSettings().setJavaScriptEnabled(true);
@@ -55,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
                 if (bus != -1) {
                     Token = Cookie.substring(bus, Cookie.indexOf(59, bus)).replace(COOKIE_TOKEN+"=", "");
                     edToken.setText(Token);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("token", Token);
+                    editor.apply();
                 }
             }
             return false;
